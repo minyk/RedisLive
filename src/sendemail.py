50255fa38d2126d6a26289463ed128bc6d5a66f2
monkeymq <legendof01@gmail.com>
2017-08-24 14:21:46 +0800

    Create sendemail.py

---
+++ src/sendemail.py
@@ -0,0 +1,21 @@
+#-*- coding: UTF-8 -*-
+
+from email.header import Header
+from email.mime.text import MIMEText
+
+import smtplib
+
+
+def send(fromAddr, toAddr, smtpServer, content):
+    msg = msg = MIMEText('<html><body>' + content + '</body></html>', 'html', 'utf-8')
+    msg['From'] = fromAddr
+    msg['To'] = toAddr
+    msg['Subject'] = Header('Redis Alter...', 'utf-8').encode()
+
+    server = smtplib.SMTP(smtpServer)
+    # server.set_debuglevel(1)
+    # server.ehlo()
+    # server.starttls()
+    # server.login(from_addr, password)
+    server.sendmail(fromAddr, toAddr, msg.as_string())
+    server.quit()

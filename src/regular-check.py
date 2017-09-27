64f057dba83dda1acb3254218875bbb59fb9c674
monkeymq <legendof01@gmail.com>
2017-09-27 02:04:26 -0500

    Update regular-check.py

--- src/regular-check.py
+++ src/regular-check.py
@@ -1,16 +1,14 @@
 #! /usr/bin/env python
 
-from apscheduler.schedulers.blocking import BlockingScheduler
 import os
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 def job():
     """
     5 minutes execute a monitor
     """
-    # duration = 60
-    # monitor = RedisMonitor()
-    # monitor.run(duration)
     os.system("python ./redis-monitor.py --duration=60")
+    #os.system("python ./redis-ping.py &")
 
 os.system("python ./redis-live.py & ")
 scheduler = BlockingScheduler()
